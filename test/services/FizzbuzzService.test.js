270786a1219bf1569a7112de276bbb3716b0b7a5
SaraiMV <sarizvargas@gmail.com>
2022-04-28 18:07:55 -0500

    Adding unit test for applyValidationInNumber method

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -71,4 +71,24 @@ describe("Unit Test for FizzbuzzService", () => {
         expect(typeof explorersWithScore.trick).toBe("number");
     });
 
+    test("5) Validate FIZZ with a input number", () => {        
+        const numberIsFizz = FizzbuzzService.applyValidationInNumber(3);
+        expect(numberIsFizz).toBe("FIZZ");
+    });
+
+    test("6) Validate BUZZ with a input number", () => {        
+        const numberIsBuzz = FizzbuzzService.applyValidationInNumber(5);
+        expect(numberIsBuzz).toBe("BUZZ");
+    });
+
+    test("7) Validate FIZZBUZZ with a input number", () => {        
+        const numberIsFizzbuzz = FizzbuzzService.applyValidationInNumber(15);
+        expect(numberIsFizzbuzz).toBe("FIZZBUZZ");
+    });
+
+    test("8) Validate is not FIZZBUZZ with a input number", () => {        
+        const numberIsNotFizzbuzz = FizzbuzzService.applyValidationInNumber(3);
+        expect(numberIsNotFizzbuzz).toBe("FIZZ");
+    });
+
 });
