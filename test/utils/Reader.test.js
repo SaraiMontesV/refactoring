6b41b03392a07c093fc0119f5b3f0e582671c8ee
SaraiMV <sarizvargas@gmail.com>
2022-04-27 22:40:06 -0500

    Adding unit test for Reader

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require("./../../lib/utils/Reader");
+
+describe("Unit Test for Reader", () => {
+  test("1) Reading a json file", () => {
+    const explorers = Reader.readJsonFile("./test/testExplorers.json");
+    expect(explorers).not.toBeNull();
+  });
+});
